23ca05cd7ce8415a69e0ece97f146575084d8261
mdayican <mustafa.dayican@hmcts.net>
2020-03-24 13:45:01 +0000

    add staging queue

--- queue.tf
+++ queue.tf
@@ -28,3 +28,28 @@ resource "azurerm_key_vault_secret" "notifications_queue_listen_conn_str" {
   name         = "notifications-queue-listen-connection-string"
   value        = "${module.notifications-queue.primary_listen_connection_string}"
 }
+
+module "notifications-queue-aat-staging" {
+  source              = "git@github.com:hmcts/terraform-module-servicebus-queue?ref=master"
+  name                = "notifications-aat-staging"
+  namespace_name      = "${module.queue-namespace.name}"
+  resource_group_name = "${azurerm_resource_group.reform_scan_rg.name}"
+  lock_duration       = "PT5M"
+
+  duplicate_detection_history_time_window = "PT15M"
+  queue_enabled       = "${var.env == "aat" ? "true" : "false" }"
+}
+
+resource "azurerm_key_vault_secret" "notifications_queue_aat_staging_send_conn_str" {
+  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+  name         = "notifications-queue-aat-staging-send-connection-string"
+  value        = "${module.notifications-queue-aat-staging.primary_send_connection_string}"
+  count        =  "${var.env == "aat" ? 1 : 0 }"
+}
+
+resource "azurerm_key_vault_secret" "notifications_queue_aat_staging_listen_conn_str" {
+  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+  name         = "notifications-queue-aat-staging-listen-connection-string"
+  value        = "${module.notifications-queue-aat-staging.primary_listen_connection_string}"
+  count        =  "${var.env == "aat" ? 1 : 0 }"
+}
