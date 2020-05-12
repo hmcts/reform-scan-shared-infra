2e2363d4544065073fb9d110a6c42508af5a4945
Nitin Prabhu <prabhunitin61@gmail.com>
2020-05-12 11:48:24 +0100

    Added app gw storage account and palo config for staging

---
+++ blob-storage-staging.tf
@@ -0,0 +1,80 @@
+locals {
+  stripped_product  = "${replace(var.product, "-", "")}"
+  account_name      = "${local.stripped_product}${var.env}staging"
+  mgmt_network_name = "core-cftptl-intsvc-vnet"
+  mgmt_network_rg_name = "aks-infra-cftptl-intsvc-rg"
+  prod_hostname     = "${local.stripped_product}.staging.${var.external_hostname}"
+  nonprod_hostname  = "${local.stripped_product}.${var.env}.staging.${var.external_hostname}"
+  external_hostname_stg = "${ var.env == "prod" ? local.prod_hostname : local.nonprod_hostname}"
+
+  // for each client service two containers are created: one named after the service
+  // and another one, named {service_name}-rejected, for storing envelopes rejected by process
+  client_containers = ["bulkscan", "cmc", "crime", "divorce", "finrem", "probate", "sscs", "publiclaw"]
+}
+
+data "azurerm_subnet" "trusted_subnet" {
+  name                 = "${local.trusted_vnet_subnet_name}"
+  virtual_network_name = "${local.trusted_vnet_name}"
+  resource_group_name  = "${local.trusted_vnet_resource_group}"
+}
+
+data "azurerm_subnet" "jenkins_subnet" {
+  provider             = "azurerm.mgmt"
+  name                 = "iaas"
+  virtual_network_name = "${local.mgmt_network_name}"
+  resource_group_name  = "${local.mgmt_network_rg_name}"
+}
+
+resource "azurerm_storage_account" "storage_account_staging" {
+  name                = "${local.account_name}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+
+  location                 = "${azurerm_resource_group.rg.location}"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "BlobStorage"
+
+  custom_domain {
+    name          = "${local.external_hostname_stg}"
+    use_subdomain = "false"
+  }
+
+  network_rules {
+    virtual_network_subnet_ids = ["${data.azurerm_subnet.trusted_subnet.id}", "${data.azurerm_subnet.jenkins_subnet.id}"]
+    bypass                     = ["Logging", "Metrics", "AzureServices"]
+    default_action             = "Deny"
+  }
+
+  tags = "${local.tags}"
+}
+
+resource "azurerm_storage_container" "client_containers" {
+  name                 = "${local.client_containers[count.index]}"
+  storage_account_name = "${azurerm_storage_account.storage_account_staging.name}"
+  count                = "${length(local.client_containers)}"
+}
+
+resource "azurerm_storage_container" "client_rejected_containers" {
+  name                 = "${local.client_containers[count.index]}-rejected"
+  storage_account_name = "${azurerm_storage_account.storage_account_staging.name}"
+  count                = "${length(local.client_containers)}"
+}
+
+# store blob storage secrets in key vault
+resource "azurerm_key_vault_secret" "storage_account_staging_name" {
+  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+  name         = "storage-account-staging-name"
+  value        = "${azurerm_storage_account.storage_account_staging.name}"
+}
+
+resource "azurerm_key_vault_secret" "storage_account_staging_primary_key" {
+  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+  name         = "storage-account-staging-primary-key"
+  value        = "${azurerm_storage_account.storage_account_staging.primary_access_key}"
+}
+
+resource "azurerm_key_vault_secret" "storage_account_staging_secondary_key" {
+  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
+  name         = "storage-account-staging-secondary-key"
+  value        = "${azurerm_storage_account.storage_account_staging.secondary_access_key}"
+}
