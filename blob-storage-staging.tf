6d9d55f6344a70b9f84a36d32adc19a1375309d0
Nitin Prabhu <prabhunitin61@gmail.com>
2020-05-14 16:29:08 +0100

    Code Review : Fix local var to use staging

--- blob-storage-staging.tf
+++ blob-storage-staging.tf
@@ -49,13 +49,13 @@ resource "azurerm_storage_account" "storage_account_staging" {
 }
 
 resource "azurerm_storage_container" "client_containers_stg" {
-  name                 = "${local.client_containers[count.index]}"
+  name                 = "${local.client_containers_stg[count.index]}"
   storage_account_name = "${azurerm_storage_account.storage_account_staging.name}"
   count                = "${length(local.client_containers_stg)}"
 }
 
 resource "azurerm_storage_container" "client_rejected_containers_stg" {
-  name                 = "${local.client_containers[count.index]}-rejected"
+  name                 = "${local.client_containers_stg[count.index]}-rejected"
   storage_account_name = "${azurerm_storage_account.storage_account_staging.name}"
   count                = "${length(local.client_containers_stg)}"
 }
