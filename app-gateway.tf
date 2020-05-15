b098c2d1119980cf003846b859191e58f30660cc
Nitin Prabhu <prabhunitin61@gmail.com>
2020-05-15 18:14:28 +0100

    Use new cert without password

--- app-gateway.tf
+++ app-gateway.tf
@@ -8,10 +8,6 @@ data "azurerm_key_vault_secret" "cert" {
   name         = "${var.external_cert_name}"
 }
 
-data "azurerm_key_vault_secret" "cert_password" {
-  name      = "${var.external_cert_name}-password"
-  key_vault_id = "${data.azurerm_key_vault.infra_vault.id}"
-}
 
 module "appGw" {
   source            = "git@github.com:hmcts/cnp-module-waf?ref=master"
@@ -34,7 +30,7 @@ module "appGw" {
     {
       name     = "${var.external_cert_name}"
       data     = "${data.azurerm_key_vault_secret.cert.value}"
-      password = "${var.env == "prod" ? "" : "${data.azurerm_key_vault_secret.cert_password.value}"}"
+      password = ""
     },
   ]
 
