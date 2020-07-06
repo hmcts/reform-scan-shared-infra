757d61b7581b316b7c6b13f1c15c8883c5098c31
apereverzin <andrey.pereverzin@hotmail.com>
2020-07-06 14:53:39 +0100

    Notifications alert

---
+++ notification-alert.tf
@@ -0,0 +1,21 @@
+// single alert to minify unnecessary cost because threshold used in here is minimal
+module "reform-scan-notification-alert" {
+  source            = "git@github.com:hmcts/cnp-module-metric-alert"
+  location          = "${azurerm_application_insights.appinsights.location}"
+  app_insights_name = "${azurerm_application_insights.appinsights.name}"
+
+  enabled    = "${var.env == "prod"}"
+  alert_name = "Reform_Scan_notification"
+  alert_desc = "Triggers when notification service sends at least 5 notifications within a 30 minutes window timeframe."
+
+  app_insights_query = "traces | where message startswith 'Sending error notification'"
+
+  frequency_in_minutes       = 15
+  time_window_in_minutes     = 30
+  severity_level             = "1"
+  action_group_name          = "${module.alert-action-group.action_group_name}"
+  custom_email_subject       = "Reform Scan notifications"
+  trigger_threshold_operator = "GreaterThan"
+  trigger_threshold          = 4
+  resourcegroup_name         = "${azurerm_resource_group.rg.name}"
+}
