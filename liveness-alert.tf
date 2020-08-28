5ba9aae34c75cda9ed7aa45bf923bea924a0e612
apereverzin <andrey.pereverzin@hotmail.com>
2020-08-28 11:36:39 +0100

    Notification service liveness alert

--- liveness-alert.tf
+++ liveness-alert.tf
@@ -22,3 +22,28 @@ EOF
   trigger_threshold          = 10
   resourcegroup_name         = "${azurerm_resource_group.rg.name}"
 }
+
+module "reform-scan-notification-service-liveness-alert" {
+  source            = "git@github.com:hmcts/cnp-module-metric-alert"
+  location          = "${azurerm_application_insights.appinsights.location}"
+  app_insights_name = "${azurerm_application_insights.appinsights.name}"
+
+  enabled    = "${var.env == "prod"}"
+  alert_name = "Reform_Scan_Notification_Service_liveness"
+  alert_desc = "Triggers when reform scan notification service looks like being down within a 30 minutes window timeframe."
+
+  app_insights_query = <<EOF
+requests
+| where name == "GET /health" and resultCode != "200"
+| where cloud_RoleName == "Reform Scan Notification Service"
+EOF
+
+  frequency_in_minutes       = 15
+  time_window_in_minutes     = 16
+  severity_level             = "2"
+  action_group_name          = "${module.alert-action-group.action_group_name}"
+  custom_email_subject       = "Reform Scan Notification Service liveness"
+  trigger_threshold_operator = "GreaterThan"
+  trigger_threshold          = 10
+  resourcegroup_name         = "${azurerm_resource_group.rg.name}"
+}
