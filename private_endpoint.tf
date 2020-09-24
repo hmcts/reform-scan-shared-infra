f06b9db1d67793c3a738d878b0c4a148e14590a5
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2020-09-24 18:19:36 +0100

    Update private_endpoint.tf

--- private_endpoint.tf
+++ private_endpoint.tf
@@ -1,5 +1,7 @@
 locals {
   scan_storage_vnet_name           = "${var.env == "aat" ? "core-infra-vnet-prod" : "core-infra-vnet-${var.env}"}"
+  private_endpoint_prod_aat_count  = "${var.env == "prod" || var.env == "aat" ? "1": "0"}"
+  private_endpoint_non_prod_count  = "${var.env == "prod" || var.env == "aat" ? "0": "1"}"
   scan_storage_vnet_resource_group = "core-infra-${var.env}"
   scan_storage_vnet_subnet_name    = "scan-storage"
 }
@@ -10,9 +12,15 @@ data "azurerm_subnet" "scan_storage_subnet" {
   resource_group_name  = "${local.scan_storage_vnet_resource_group}"
 }
 
-resource "azurerm_template_deployment" "private_endpoint" {
+provider "azurerm" {
+  alias           = "cnp-prod"
+  subscription_id = "8999dec3-0104-4a27-94ee-6588559729d1"
+}
+
+resource "azurerm_template_deployment" "private_endpoint_prod_aat" {
   name                = "${local.account_name}-endpoint"
   resource_group_name = "${azurerm_resource_group.rg.name}"
+  count               = "${local.private_endpoint_prod_aat_count}}"
 
   template_body = file("private_endpoint_template.json")
 
@@ -21,7 +29,26 @@ resource "azurerm_template_deployment" "private_endpoint" {
     endpoint_location   = "${azurerm_resource_group.rg.location}"
     subnet_id           = "${data.azurerm_subnet.scan_storage_subnet.id}"
     storageaccount_id   = "${azurerm_storage_account.storage_account.id}" 
-    storageaccount_fqdn = "${azurerm_storage_account.storage_account.primary_blob_endpoint}"
+    storageaccount_fqdn = "${azurerm_storage_account.storage_account.primary_blob_endpoint }"
+  }
+
+  deployment_mode = "Incremental"
+  provider = "cnp-prod"
+}
+
+resource "azurerm_template_deployment" "private_endpoint_non_prod" {
+  name                = "${local.account_name}-endpoint"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  count               = "${local.private_endpoint_non_prod_count}}"
+
+  template_body = file("private_endpoint_template.json")
+
+  parameters = {
+    endpoint_name       = "${local.account_name}-endpoint"
+    endpoint_location   = "${azurerm_resource_group.rg.location}"
+    subnet_id           = "${data.azurerm_subnet.scan_storage_subnet.id}"
+    storageaccount_id   = "${azurerm_storage_account.storage_account.id}" 
+    storageaccount_fqdn = "${azurerm_storage_account.storage_account.primary_blob_endpoint }"
   }
 
   deployment_mode = "Incremental"
