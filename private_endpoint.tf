097a870504d89b4b20114149a283ee90ec3bdba7
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2020-09-24 18:57:26 +0100

    Update private_endpoint.tf

--- private_endpoint.tf
+++ private_endpoint.tf
@@ -1,59 +1,25 @@
-locals {
-  scan_storage_vnet_name           = "${var.env == "aat" ? "core-infra-vnet-prod" : "core-infra-vnet-${var.env}"}"
-  private_endpoint_prod_aat_count  = "${var.env == "prod" || var.env == "aat" ? "1": "0"}"
-  private_endpoint_non_prod_count  = "${var.env == "prod" || var.env == "aat" ? "0": "1"}"
-  scan_storage_vnet_resource_group = "${var.env == "aat" ? "core-infra-prod" : "core-infra-${var.env}"}"
-  scan_storage_vnet_subnet_name    = "scan-storage"
-}
-
-provider "azurerm" {
-  alias           = "cnp-prod"
-  subscription_id = "8999dec3-0104-4a27-94ee-6588559729d1"
-}
-
-data "azurerm_subnet" "scan_storage_subnet_prod" {
-  name                 = "${local.scan_storage_vnet_subnet_name}"
-  virtual_network_name = "${local.scan_storage_vnet_name}"
-  resource_group_name  = "${local.scan_storage_vnet_resource_group}"
-  provider              = "azurerm.cnp-prod"
-}
-
-data "azurerm_subnet" "scan_storage_subnet" {
-  name                 = "${local.scan_storage_vnet_subnet_name}"
-  virtual_network_name = "${local.scan_storage_vnet_name}"
-  resource_group_name  = "${local.scan_storage_vnet_resource_group}"
-}
-
-resource "azurerm_template_deployment" "private_endpoint_prod_aat" {
-  name                = "${local.account_name}-endpoint"
+resource "azurerm_virtual_network" "storage_vnet" {
+  name                = "${local.account_name}-vnet"
   resource_group_name = "${azurerm_resource_group.rg.name}"
-  count               = "${local.private_endpoint_prod_aat_count}}"
+  location            = "${azurerm_resource_group.rg.location}"
+  address_space       = ["${var.storage_vnet_cidr}"]
 
-  template_body = file("private_endpoint_template.json")
-
-  parameters = {
-    endpoint_name       = "${local.account_name}-endpoint"
-    endpoint_location   = "${azurerm_resource_group.rg.location}"
-    subnet_id           = "${data.azurerm_subnet.scan_storage_subnet.id}"
-    storageaccount_id   = "${azurerm_storage_account.storage_account.id}" 
-    storageaccount_fqdn = "${azurerm_storage_account.storage_account.primary_blob_endpoint }"
+  subnet {
+    name           = "scan-storage"
+    address_prefix = "${var.storage_subnet_cidr}"
   }
-
-  deployment_mode       = "Incremental"
-  provider              = "azurerm.cnp-prod"
 }
 
-resource "azurerm_template_deployment" "private_endpoint_non_prod" {
+resource "azurerm_template_deployment" "private_endpoint" {
   name                = "${local.account_name}-endpoint"
   resource_group_name = "${azurerm_resource_group.rg.name}"
-  count               = "${local.private_endpoint_non_prod_count}"
 
   template_body = file("private_endpoint_template.json")
 
   parameters = {
     endpoint_name       = "${local.account_name}-endpoint"
     endpoint_location   = "${azurerm_resource_group.rg.location}"
-    subnet_id           = "${data.azurerm_subnet.scan_storage_subnet_prod.id}"
+    subnet_id           = "${azurerm_virtual_network.storage_vnet.scan-storage.id}"
     storageaccount_id   = "${azurerm_storage_account.storage_account.id}" 
     storageaccount_fqdn = "${azurerm_storage_account.storage_account.primary_blob_endpoint }"
   }
