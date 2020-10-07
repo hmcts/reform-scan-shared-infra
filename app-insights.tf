23b7fc9ddd2aaadb657649292a9c660bc1f75972
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2020-10-07 16:10:26 +0100

    Update app-insights.tf

--- app-insights.tf
+++ app-insights.tf
@@ -1,14 +1,25 @@
-resource "azurerm_application_insights" "appinsights" {
-  name                = "${var.product}-${var.env}"
-  location            = "${var.location}"
-  resource_group_name = "${azurerm_resource_group.rg.name}"
-  application_type    = "${var.application_type}"
-  tags                = "${var.common_tags}"
-}
-
-# store app insights key in key vault
-resource "azurerm_key_vault_secret" "appinsights_secret" {
+resource "azurerm_key_vault_secret" "AZURE_APPINSIGHTS_KEY" {
   name         = "app-insights-instrumentation-key"
   value        = "${azurerm_application_insights.appinsights.instrumentation_key}"
   key_vault_id = "${module.vault.key_vault_id}"
+}
+
+resource "azurerm_application_insights" "appinsights" {
+  name                = "${var.product}-${var.env}"
+  location            = "${var.appinsights_location}"
+  resource_group_name = "${azurerm_resource_group.rg.name}"
+  application_type    = "${var.application_type}"
+  tags                = "${var.common_tags}"
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
+      # destroys and re-creates this appinsights instance
+      application_type,
+    ]
+  }
+}
+
+output "appInsightsInstrumentationKey" {
+  value = "${azurerm_application_insights.appinsights.instrumentation_key}"
 }
