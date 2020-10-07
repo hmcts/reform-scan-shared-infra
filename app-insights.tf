278dd573f1c9c10b990c892182a7cd29ca56cb22
Adam Komisarek <adam.komisarek@hmcts.net>
2020-10-07 14:48:48 +0100

    fix: moved the lifecycle to proper resource

--- app-insights.tf
+++ app-insights.tf
@@ -4,13 +4,6 @@ resource "azurerm_application_insights" "appinsights" {
   resource_group_name = "${azurerm_resource_group.rg.name}"
   application_type    = "${var.application_type}"
   tags                = "${var.common_tags}"
-}
-
-# store app insights key in key vault
-resource "azurerm_key_vault_secret" "appinsights_secret" {
-  name         = "app-insights-instrumentation-key"
-  value        = "${azurerm_application_insights.appinsights.instrumentation_key}"
-  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
 
   lifecycle {
     ignore_changes = [
@@ -19,4 +12,11 @@ resource "azurerm_key_vault_secret" "appinsights_secret" {
       application_type,
     ]
   }
+}
+
+# store app insights key in key vault
+resource "azurerm_key_vault_secret" "appinsights_secret" {
+  name         = "app-insights-instrumentation-key"
+  value        = "${azurerm_application_insights.appinsights.instrumentation_key}"
+  key_vault_id = "${data.azurerm_key_vault.key_vault.id}"
 }
