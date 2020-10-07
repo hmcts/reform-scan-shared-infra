e720a6f6e068a77f29261a6ebcf675f1bad7ce57
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2020-10-07 16:17:04 +0100

    Update app-insights.tf

--- app-insights.tf
+++ app-insights.tf
@@ -6,7 +6,7 @@ resource "azurerm_key_vault_secret" "AZURE_APPINSIGHTS_KEY" {
 
 resource "azurerm_application_insights" "appinsights" {
   name                = "${var.product}-${var.env}"
-  location            = "${var.appinsights_location}"
+  location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.rg.name}"
   application_type    = "${var.application_type}"
   tags                = "${var.common_tags}"
