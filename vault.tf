07ddbeb462c86233122765a97edd7114eebc98da
James Johnson <2965841+jrjohnson87@users.noreply.github.com>
2020-10-17 13:34:57 +0100

    Update vault.tf

--- vault.tf
+++ vault.tf
@@ -1,6 +1,6 @@
 module "vault" {
   source                  = "git@github.com:hmcts/cnp-module-key-vault?ref=azurermv2"
-  name                    = "${var.product}-v2-ptest"
+  name                    = "${var.product}-ptest"
   product                 = "${var.product}"
   env                     = "${var.env}"
   tenant_id               = "${var.tenant_id}"
