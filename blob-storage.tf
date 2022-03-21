2bcd1a36607f357fec86c0f94c709da4c83f794c
Enda Kelly <enda.kelly@hmcts.net>
2022-03-21 10:08:40 +0000

    remove more quotes

--- blob-storage.tf
+++ blob-storage.tf
@@ -26,14 +26,14 @@ data "azurerm_subnet" "jenkins_subnet" {
 }
 
 data "azurerm_subnet" "aks_00_subnet" {
-  provider             = "azurerm.mgmt"
+  provider             = azurerm.mgmt
   name                 = "aks-00"
   virtual_network_name = local.mgmt_network_name
   resource_group_name  = local.mgmt_network_rg_name
 }
 
 data "azurerm_subnet" "aks_01_subnet" {
-  provider             = "azurerm.mgmt"
+  provider             = azurerm.mgmt
   name                 = "aks-01"
   virtual_network_name = local.mgmt_network_name
   resource_group_name  = local.mgmt_network_rg_name
