d123118fa85f330c65e8621a021083170aa6fba9
Enda Kelly <enda.kelly@hmcts.net>
2022-03-22 14:22:23 +0000

    fix typo

--- private_endpoint.tf
+++ private_endpoint.tf
@@ -29,7 +29,7 @@ resource "azurerm_private_endpoint" "private_endpoint" {
   }
 
   private_dns_zone_group {
-    name                         = data.azurerm_private_dns_zone.private_link_dns_zone.name
-    private_private_dns_zone_ids = data.azurerm_private_dns_zone.private_link_dns_zone.id
+    name                 = data.azurerm_private_dns_zone.private_link_dns_zone.name
+    private_dns_zone_ids = data.azurerm_private_dns_zone.private_link_dns_zone.id
   }
 }
