853624dd22a384e2ff8fcdb84413d5c96752daa4
msl8r <72858053+msl8r@users.noreply.github.com>
2022-04-01 09:23:32 +0100

    DTSPO-6371 add provider for service bus

--- provider.tf
+++ provider.tf
@@ -4,3 +4,10 @@ provider "azurerm" {
   subscription_id = "ed302caf-ec27-4c64-a05e-85731c3ce90e"
   features {}
 }
+
+provider "azurerm" {
+  features {}
+  skip_provider_registration = true
+  alias                      = "private-endpoint"
+  subscription_id            = var.aks_subscription_id
+}
--- queue-premium.tf
+++ queue-premium.tf
@@ -1,4 +1,8 @@
 module "queue-namespace-premium" {
+  providers = {
+    azurerm.private-endpoint = azurerm.private-endpoint
+  }
+
   source              = "git@github.com:hmcts/terraform-module-servicebus-namespace?ref=master"
   name                = "${local.product}-servicebus-${var.env}-premium"
   location            = var.location
